ac279827865864ae49a257f80e7add3b43fe7ded
Christian M <chriamue@gmail.com>
2020-04-25 08:12:43 +0200

    updated imports

--- android/src/main/java/com/dpppt/DppptModule.java
+++ android/src/main/java/com/dpppt/DppptModule.java
@@ -1,11 +1,5 @@
 package com.dpppt;
 
-import android.util.Base64;
-import java.nio.charset.StandardCharsets;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -13,7 +7,6 @@ import com.facebook.react.bridge.Callback;
 
 import org.dpppt.android.sdk.DP3T;
 import org.dpppt.android.sdk.internal.backend.CallbackListener;
-import org.dpppt.android.sdk.internal.backend.models.ExposeeAuthData;
 
 public class DppptModule extends ReactContextBaseJavaModule {
 
@@ -51,20 +44,14 @@ public class DppptModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void sendIWasExposed() {
-        Calendar calendarNow = new GregorianCalendar();
-        calendarNow.add(Calendar.DAY_OF_YEAR, -14);
-        String inputBase64 = new String(
-                Base64.encode(authCodeInput.getText().getBytes(StandardCharsets.UTF_8), Base64.NO_WRAP),
-                StandardCharsets.UTF_8);
-        DP3T.sendIWasExposed(getReactApplicationContext(), new Date(calendarNow.getTimeInMillis()),
-                new ExposeeAuthData(inputBase64), new CallbackListener<Void>() {
-                    @Override
-                    public void onSuccess(Void response) {
-                    }
+        DP3T.sendIWasExposed(getReactApplicationContext(), null, new CallbackListener<Void>() {
+            @Override
+            public void onSuccess(Void response) {
+            }
 
-                    @Override
-                    public void onError(Throwable throwable) {
-                    }
-                });
+            @Override
+            public void onError(Throwable throwable) {
+            }
+        });
     }
 }
